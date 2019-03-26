ca2f0b79cefe873e14e2f2e268654797da716d25
ChanChunCheong <chanchuncheong.cjz@gmail.com>
2019-03-26 16:17:10 +0800

    -view completed task and view pending tasks for taskrequesters work

--- routes/taskRequesters.js
+++ routes/taskRequesters.js
@@ -13,46 +13,66 @@ const pool = new Pool({
 });
 pool.connect();
 
-router.get("/", (req, res) => {
-    res.send(
-      "IF Id exist in taskers table, load tasker info, else redirect to create profile page"
-    );
-    /*
-    if((select name from taskers where ))
-    */
-  });
+router.get("/",ensureAuthenticated, (req, res) => {
+    //Retrieve all tasks and send along with render
+    var cusId = parseInt(req.user.cusId)
+    const param1 = [cusId];
+    //console.log(cusId);
+    const sql1 = "SELECT * FROM customers WHERE cusid = $1"
+    pool.query(sql1,param1, (err, result1) => {
+      if(err){
+        console.log("ERROR RETRIEVING Customer");
+      } else {
+        res.render('taskReqProfile',{cusInfo: result1.rows});
+      }
+  
+    });
+    
+});
   
 // Start: CRUD Listings
 
+//Add route
+router.get("/addListings", function(req, res) {
+    res.render("add_Listings");
+});
+  
 router.post("/addListings", (req, res) => {
-//   console.log(req.body.catName);
-//   console.log(req.body.description);
-//   console.log(req.body.rate);
+    req.checkBody("taskId", "Id is required").notEmpty();
+    req.checkBody("taskName", "Category Name is required").notEmpty();
+    req.checkBody("description", "Description is required").notEmpty();
+    req.checkBody("duration", "Duration is required").notEmpty();
+    req.checkBody("taskDateTime", "taskDateTime is required").notEmpty();
+    req.checkBody("dateCreated", "Date created is required").notEmpty();
+    req.checkBody("deadline", "Deadline is required").notEmpty();
+    req.checkBody("startingBid", "Starting bid is required").notEmpty();
+    let errors = req.validationErrors();
+    if (errors) {
+        res.render('add_category');
+        console.log(errors);
 
-//Insert into CreatedTasks (taskId, taskName, Description, Duration, Manpower, taskDateTime, dateCreated, Completed, cusID  ) 
+    } else {
+        const sql1 = "INSERT INTO CreatedTasks (catId, catName) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+        const params1 = [req.body.taskId, req.body.taskName, req.body.taskName, req.body.description, req.body.duration, req.body.taskDateTime, req.body.dateCreated, req.user.cusId];
+        pool.query(sql1, params1, (error, result) => {
+            if (error) {
+                console.log("err: ", error);
+            }
+        });
 
-req.checkBody("Task Name", "description is required").notEmpty();
-req.checkBody("description", "description is required").notEmpty();
-req.checkBody("rate", "rate is required").notEmpty();
-req.checkBody("date", "description is required").notEmpty();
-req.checkBody("description", "description is required").notEmpty();
-req.checkBody("catName", "Category Name is required").notEmpty();
-
-let error = req.validationErrors();
-if (error) {
-    res.render("/");
-    console.log("err: ", error);
-} else {
-    const paramSkill = ["1", "1", req.body.description, req.body.rate];
-    const sqlAddSkill =
-    "INSERT INTO AddedPersonalSkills(ssid,cusId,description,ratePerHour) VALUES($1,$2,$3,$4)";
-    pool.query(sqlAddSkill, paramSkill, (error, result) => {
-    if (error) {
-        console.log("err: ", error);
+        const sql2 = "INSERT INTO Listings (biddingDeadlin, startingBid, taskId) VALUES ($1, $2, $3)";
+        const params2 = [req.body.deadline, req.body.startingBid, req.body.taskId];
+        pool.query(sql2, params2, (error, result) => {
+            if (error) {
+                console.log("err: ", error);
+            }
+            //req.flash('success', 'Article Added');
+            console.log("result?", result);
+            res.redirect("/");
+        });
     }
-    res.redirect("/");
-    });
-}
+
+});
 
 router.get("/viewListings", (req, res) => {
     res.send("Retrieve all Listings that has not expired  ");
@@ -62,7 +82,7 @@ router.get("/viewListings", (req, res) => {
         WHERE taskDateTime > (SELECT NOW())
         AND C.cusId = T.cusId
         VALUES ($1, $2, $3, $4, $5)"; */
-    });
+});
     
 
 router.put("/updateListings", (req, res) => {
@@ -83,7 +103,7 @@ router.put("/updateListings", (req, res) => {
         "UPDATE Belongs
         SET catId = (SELECT catId FROM Categories WHERE name = "Category")"
     "; */
-    });
+});
     
 router.delete("/deleteListings", (req, res) => {
 res.send("Execute Delete SQL Statementm then redirect to /viewSkillsets");
@@ -131,7 +151,7 @@ const sqlRetrieveSkills =
 //End: CRUD Requests 
 
 //View all Tasks
-router.get('/viewMyCompletedTasks', function (req, res) {
+router.get('/viewAllTasks', function (req, res) {
     const sql = 'SELECT taskname, description, duration, manpower, taskdatetime, datecreated FROM createdTasks C join assigned A on (C.taskid = A.taskid AND A.cusid = $1 AND A.completed = true)' 
     const params = [parseInt(req.user.cusId)]
     
@@ -141,34 +161,37 @@ router.get('/viewMyCompletedTasks', function (req, res) {
             console.log('err: ', error);
         }
   
-        res.render('view_my_tasks', {
+        res.render('view_tr_all_tasks', {
             task: result.rows,
             taskType: 'COMPLETED'
         });
   
     });
+});
 
 //View all my completed Tasks
-router.get('/viewMyCompletedTasks', function (req, res) {
-    const sql = 'SELECT taskname, description, duration, manpower, taskdatetime, datecreated FROM createdTasks C join assigned A on (C.taskid = A.taskid AND A.cusid = $1 AND A.completed = true)' 
-    const params = [parseInt(req.user.cusId)]
+router.get('/viewCompletedTasks', function (req, res) {
     
+    const params = [parseInt(req.user.cusId)]
+    const sql = 'select taskname, description, duration, manpower, taskdatetime, datecreated from CreatedTasks C join assigned A on C.taskid = A.taskid where C.cusId = $1 and A.completed = true' 
+
     pool.query(sql, params, (error, result) => {
     
         if (error) {
             console.log('err: ', error);
         }
   
-        res.render('view_my_tasks', {
+        res.render('view_tr_completed_tasks', {
             task: result.rows,
             taskType: 'COMPLETED'
         });
   
     });
+});
 
 //View all my pending Tasks
-router.get('/viewMyPendingTasks', function (req, res) {
-    const sql = 'SELECT taskname, description, duration, manpower, taskdatetime, datecreated FROM createdTasks C join assigned A on (C.taskid = A.taskid AND A.cusid = $1 AND A.completed = false)' 
+router.get('/viewPendingTasks', function (req, res) {
+    const sql = 'select taskname, description, duration, manpower, taskdatetime, datecreated from CreatedTasks C join assigned A on C.taskid = A.taskid where C.cusId = $1 and A.completed = false'
     const params = [parseInt(req.user.cusId)]
     
     pool.query(sql, params, (error, result) => {
@@ -177,44 +200,60 @@ router.get('/viewMyPendingTasks', function (req, res) {
             console.log('err: ', error);
         }
   
-        res.render('view_my_tasks', {
+        res.render('view_tr_pending_tasks', {
             task: result.rows,
             taskType: 'PENDING'
         });
   
     });
-  
+});
+
+//select task to show bids
+router.get('/selectTaskBid', ensureAuthenticated, function (req, res) {
+    res.render('select_bid_list');
+});
+
 //View all biddings for the list
-router.get('/viewMyBids', ensureAuthenticated, function (req, res) {
-    const sql = 'SELECT C.taskName, B.bidPrice, L.biddingDeadline FROM CreatedTasks C join (Listings L join Bids B on (L.taskId = B.taskId)) on (C.taskId = L.taskId AND B.cusId = $1)'
-    const params = [parseInt(req.user.cusId)]
-    
-    pool.query(sql, params, (error, result) => {
-    
-        if (error) {
-            console.log('err: ', error);
-        }
-  
-        res.render('view_my_bids', {
-            bid: result.rows,
-            
+router.get('/viewBids', ensureAuthenticated, function (req, res) {
+
+    req.checkBody("taskName", "Id is required").notEmpty();
+    let errors = req.validationErrors();
+    if (errors) {
+        res.render('add_category');
+        console.log(errors);
+
+    } else {
+        const sql = 'SELECT B.bidprice, C.name FROM (listings L join bids B on L.taskid = B.taskid) join customers C on B.cusid = C.cusid where L.taskid = $1' 
+        const params = [req.body.taskName];
+        //need to set this to strings 
+        pool.query(sql, params, (error, result) => {
+
+            if (error) {
+                console.log('err: ', error);
+            }
+
+            res.render('view_tr_bid_tasks', {
+                bid: result.rows,
+                bidType: req.body.taskName
+            });
+
         });
-  
-    });
-  
+    }
+});
+
     //Access Control
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
-  
+
     } else {
         req.flash('danger', 'Please Log In');
         
         res.redirect('/users/login');
     }
-  
-  }
-  
+
+}
+
   
 
 module.exports = router;
